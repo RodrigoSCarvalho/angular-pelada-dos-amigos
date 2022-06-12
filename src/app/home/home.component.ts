42601d18adb795a50225d442e7e3dd95497fd3d6
RodrigoSCarvalho <carvalho_rodrigo@id.uff.br>
2022-06-12 00:00:21 -0300

    Correção no remover jogador

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,70 +1,81 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule  } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Jogador } from '../models/Jogador';
 import { JogadorService } from '../services/jogador.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private _jogadorService: JogadorService) {
-    
-   }
+  constructor(private _jogadorService: JogadorService) {}
 
   @ViewChild('jogador') inputJogador: any;
-  @ViewChild('grid') divGrid: any;
+  //@ViewChild('grid') divGrid: any;
   jogadores: Jogador[] = [];
   players: string[] = [];
   isVisible: boolean = true;
- 
+  removidos: number[] = [];
+  maxJog: number = 20;
+
   ngOnInit(): void {
     this.retrieveAllJogadores();
   }
 
   retrieveAllJogadores(): void {
     this._jogadorService.retrieveAllJogadores().subscribe({
-      next: (jogador: any)=>{
-      this.jogadores = jogador;
-    },error: (err) => {
-      alert('Error :'+ err);
-    },}
-
-    );
+      next: (jogador: any) => {
+        this.jogadores = jogador;
+      },
+      error: (err) => {
+        alert('Error :' + err);
+      },
+    });
   }
 
   onClick(nome: string): void {
-    if("" == nome || nome == " "){
-      
+    if ('' == nome || nome == ' ') {
+    } else {
+      if (this.players.length < this.maxJog) {
+        this.players.push(nome);
+        console.log(this.players.length);
+      }
+      if (this.players.length == this.maxJog) {
+        this.inputJogador.nativeElement.setAttribute('readonly', true);
+        this.inputJogador.nativeElement.setAttribute(
+          'placeholder',
+          'Todos os 20 jogadores foram inseridos!'
+        );
+      }
+      this.inputJogador.nativeElement.value = '';
     }
-    else{
-      
-    if (this.players.length < 20){
-      this.players.push(nome);      
-      
-    }
-    if(this.players.length == 20){
-      this.inputJogador.nativeElement.setAttribute('readonly', true,)
-      this.inputJogador.nativeElement.setAttribute('placeholder', "Todos os 20 jogadores foram inseridos!");
-    }
-    this.inputJogador.nativeElement.value = '';
-    
   }
-}
 
   definirPotes(): void {
-    for (let jogador in this.players) {
-      this._jogadorService.postJogador({id: 0, nome: this.players[jogador]}).subscribe();
-      console.log(jogador)
+    this.removidos.sort();
+    for (let k = 0; k < this.removidos.length; k++) {
+      console.log('vou remover: ' + this.players[this.removidos[k]]);
+      delete this.players[this.removidos[k]];
+      console.log(this.players);
     }
+    for (let jogador in this.players) {
+      //this._jogadorService
+        // .postJogador({ id: 0, nome: this.players[jogador] })
+        // .subscribe();
+    }
+    console.log(this.players);
   }
 
   removeJogador(event: any, index: number): void {
     (<HTMLInputElement>event.target)?.parentElement?.parentElement?.remove();
-    this.players.splice(index, 1);
-    console.log(this.players)
-  }
 
+    this.removidos.push(index);
+    //this.players.splice(index, 1);
+    this.maxJog + 1;
+
+    if (this.players.length >= this.maxJog -1 )
+      this.inputJogador.nativeElement.setAttribute('readonly', false);
+    this.inputJogador.nativeElement.setAttribute('placeholder', 'Ex: Rodrigo');
+  }
 }
